301e12d04c597ad6de83299d80e3f6b936400474
zul132 <fathimazulaikha.25cs@licet.ac.in>
2024-11-20 22:53:12 +0530

    updated backend url in AxiosInstance

--- frontend/src/components/common/AxiosInstance.jsx
+++ frontend/src/components/common/AxiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000', 
+  baseURL: 'http://localhost:5000', // Existing base URL
 });
 
 export default axiosInstance;
